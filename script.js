f5b8b70a2446d00df83885742bc72c1c6802f9c7
PietroCampodonico <pietro.campodonico@outlook.it>
2021-05-12 13:02:29 +0200

    API query successful

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 new Vue({
     el: "#app",
     data: {
+        myJam: [],
+    },
 
+    mounted() {
+
+        axios.get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then((resp) => {
+            this.myJam.push(resp.data.response)
+        })
     },
 })
