c92ce308000815cce163991a0bd632e6c6f5a109
PietroCampodonico <pietro.campodonico@outlook.it>
2021-05-12 16:25:52 +0200

    inserito v-for per generare albums

--- script.js
+++ script.js
@@ -2,13 +2,32 @@ new Vue({
     el: "#app",
     data: {
         myJam: [],
+        albumList: [],
     },
 
     mounted() {
 
+        let mockArray = [];
+        let filteredMockArray = [];
+
+
         axios.get("https://flynn.boolean.careers/exercises/api/array/music")
         .then((resp) => {
-            this.myJam.push(resp.data.response)
+            mockArray.push(resp.data.response)
+            this.myJam.push(...mockArray);
+
+            
+            this.myJam.map((element) => {
+                element.forEach(album => {
+                    filteredMockArray.push(album)
+                });
+            })
+            
+            this.albumList.push(...filteredMockArray)
+
+            console.log(this.myJam)
+            console.log(this.albumList)
+
         })
     },
 })
